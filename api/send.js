636a9dc2e2f1061b3171bbd240fe51952651560b
eduardobrandaodev <131871638+eduardobrandaodev@users.noreply.github.com>
2025-10-22 19:04:49 -0300

    g

--- api/send.js
+++ api/send.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const phone = process.env.PHONE;
   const mensagem = process.env.MENSAGEM;
 
-  const url = `https://api.z-api.io/instances/${instanceId}/token/${token}/send-messages`;
+  const url = `https://api.z-api.io/instances/${instanceId}/token/${token}/send-text`;
 
   try {
     const response = await fetch(url, {
@@ -17,6 +17,8 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
+
+    // Retorna o resultado da API
     return res.status(200).json({ success: true, data });
   } catch (error) {
     return res.status(500).json({ success: false, error: error.message });
